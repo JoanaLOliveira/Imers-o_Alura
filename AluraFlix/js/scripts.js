cb6267a05e09826bcfa42a2b00e40cf5d27fc30f
JoanaLOliveira <joanalmarcolino@gmail.com>
2023-11-15 09:43:49 -0300

    Alura Flix 15/11

---
+++ AluraFlix/js/scripts.js
@@ -0,0 +1,5 @@
+var listaFilmes = ["https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg","https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg","https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg"];
+for (var i = 0; i < listaFilmes.length; i++){
+    document.write("<img src=" + listaFilmes[i] + ">");
+}
+
